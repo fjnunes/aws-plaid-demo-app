2be15e69903c0c484cafdbebcadf0b6998c026b6
Fernando Nunes <fernando.nunes@lab360.ai>
2024-05-26 21:12:45 -0400

    adding statements download feature

--- frontend/src/pages/Protected.js
+++ frontend/src/pages/Protected.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/api';
 import { ConsoleLogger } from 'aws-amplify/utils';
-import { View, Heading, Flex } from '@aws-amplify/ui-react';
+import { View, Heading, Flex, Button } from '@aws-amplify/ui-react';
 import { getItems as GetItems } from '../graphql/queries';
 import Plaid from '../components/Plaid';
 import Institutions from '../components/Institutions';
@@ -24,21 +24,41 @@ export default function Protected() {
     }
   }
 
+  const downloadStatements = async () => {
+    try {
+      const response = await fetch('/v1/statements/download', {
+        method: 'GET',
+      });
+
+      if (response.status === 302) {
+        const redirectUrl = response.headers.get('Location');
+        window.location.href = redirectUrl;
+      } else {
+        logger.error('Failed to get download URL', await response.json());
+      }
+    } catch (err) {
+      logger.error('Error downloading statements', err);
+    }
+  }
+
   useEffect(() => {
     getItems();
   }, []);
 
   return (
     <Flex direction="column">
-      <Plaid label="Get statements" product="statements" getItems={getItems}/>
+      <Plaid label="Add Account" product="statements" getItems={getItems} />
+      
+      <Button onClick={downloadStatements}>Download Statements</Button>
 
       {(items && items.length) ? (
         <View>
           <Heading>Institutions</Heading>
-          <Institutions institutions={items}/>
+          <Institutions institutions={items} />
         </View>
-      ) : (<div/>)
-      }
+      ) : (
+        <div />
+      )}
     </Flex>
   );
 }
